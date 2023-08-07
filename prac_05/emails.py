f80c4b6fd03d51b6721f5443cd628a3b7b509c90
Tarshan_testing <vicknesantarshan@gmail.com>
2023-08-07 15:11:31 +0800

    cp1404practicals

---
+++ prac_05/emails.py
@@ -0,0 +1,28 @@
+def main():
+    user_data = {}
+    email = input("Email: ").strip()
+
+    while email != "":
+        name = extract_name(email)
+        is_correct = input(f"Is your name {name}? (Y/n) ").strip().lower()
+
+        if is_correct != "y" and is_correct != "":
+            name = input("Name: ").strip().title()
+
+        user_data[email] = name
+        email = input("Email: ").strip()
+
+    print("\nName and Email pairs:")
+    for email, name in user_data.items():
+        print(f"{name} ({email})")
+
+
+def extract_name(email):
+    name = email.split('@')[0]
+    name = ''.join(c for c in name if c.isalpha() or c.isspace())
+    name = name.title()
+    return name
+
+
+main()
+
