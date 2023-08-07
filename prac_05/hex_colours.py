29e57358631814899443caf3cfc2b33f42ebc3c3
Tarshan_testing <vicknesantarshan@gmail.com>
2023-08-07 14:40:19 +0800

    cp1404practicals

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -11,21 +11,9 @@ COLORS = {
     "BLUE": "#0000ff"
 }
 
-
-def main():
-    color_name = input("Enter color name (press Enter to exit): ")
-    while color_name:
-        color_code = get_color_code(color_name)
-        print(f"The hexadecimal code for {color_name} is {color_code}")
-        color_name = input("Enter color name (press Enter to exit): ")
-
-
-def get_color_code(color_name):
-    # Convert the input color_name to uppercase for case-insensitivity
-    color_name = color_name.upper()
-
-    # Look up the color_name in the COLORS dictionary
-    return COLORS.get(color_name, "Invalid color name")
-
-
-main()
+colour_name = input("Enter a colour name: ")
+while colour_name != "":
+    # Note: using the get dictionary method
+    # means you will get None if the key is not found
+    print(f"The code for \"{colour_name}\" is {COLORS.get(colour_name)}")
+    colour_name = input("Enter a colour name: ")
