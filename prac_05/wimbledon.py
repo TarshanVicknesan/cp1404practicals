5908c2580684bc6560e98ee8f521ce8d55482182
Tarshan_testing <vicknesantarshan@gmail.com>
2023-08-12 10:52:13 +0800

    cp1404practicals

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,14 +4,12 @@ INDEX_CHAMPION = 2
 
 
 def main():
-    """Read data file and print details about Wimbledon champions and countries."""
     records = get_records(FILENAME)
     champion_to_count, countries = process_records(records)
     display_results(champion_to_count, countries)
 
 
 def process_records(records):
-    """Create dictionary of champions and set of countries from records (list of lists)."""
     champion_to_count = {}
     countries = set()
     for record in records:
@@ -24,7 +22,6 @@ def process_records(records):
 
 
 def display_results(champion_to_count, countries):
-    """Display champions and countries"""
     print("Wimbledon Champions: ")
     for name, count in champion_to_count.items():
         print(name, count)
@@ -33,7 +30,6 @@ def display_results(champion_to_count, countries):
 
 
 def get_records(filename):
-    """Get records from file in list of lists form."""
     records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()  # Remove header
