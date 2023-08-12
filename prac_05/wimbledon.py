18a1e97b2e16a0e4a7d3d01d09933b60daa738e6
Tarshan_testing <vicknesantarshan@gmail.com>
2023-08-12 20:46:41 +0800

    cp1404practicals

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,42 +1,54 @@
-FILENAME = "wimbledon.csv"
-INDEX_COUNTRY = 1
-INDEX_CHAMPION = 2
+CSV_FILENAME = "wimbledon.csv"
+COUNTRY_COLUMN = 1
+CHAMPION_COLUMN = 2
 
 
+# Main function
 def main():
-    records = get_records(FILENAME)
-    champion_to_count, countries = process_records(records)
-    display_results(champion_to_count, countries)
+    records = read_csv_records(CSV_FILENAME)
+    champion_counts, countries = process_records(records)
+    display_results(champion_counts, countries)
 
 
+# Process records function
 def process_records(records):
-    champion_to_count = {}
-    countries = set()
+    champion_counts = {}
+    unique_countries = set()
+
     for record in records:
-        countries.add(record[INDEX_COUNTRY])
-        try:
-            champion_to_count[record[INDEX_CHAMPION]] += 1
-        except KeyError:
-            champion_to_count[record[INDEX_CHAMPION]] = 1
-    return champion_to_count, countries
+        unique_countries.add(record[COUNTRY_COLUMN])
+        if record[CHAMPION_COLUMN] in champion_counts:
+            champion_counts[record[CHAMPION_COLUMN]] += 1
+        else:
+            champion_counts[record[CHAMPION_COLUMN]] = 1
+
+    return champion_counts, unique_countries
 
 
-def display_results(champion_to_count, countries):
-    print("Wimbledon Champions: ")
-    for name, count in champion_to_count.items():
-        print(name, count)
-    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
-    print(", ".join(country for country in sorted(countries)))
+# Display results function
+def display_results(champion_counts, countries):
+    print("Wimbledon Champions:")
+    for champion, count in champion_counts.items():
+        print(f"{champion}: {count} wins")
+
+    countries_string = ", ".join(sorted(countries))
+    print("\nCountries of champions in alphabetical order:")
+    print(countries_string)
 
 
-def get_records(filename):
+# Read CSV records function
+def read_csv_records(filename):
     records = []
-    with open(filename, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline()  # Remove header
-        for line in in_file:
+    with open(filename, "r", encoding="utf-8-sig") as csv_file:
+        next(csv_file)  # Skip header line
+        for line in csv_file:
             parts = line.strip().split(",")
             records.append(parts)
     return records
 
 
+# Call the main function
 main()
+
+
+
