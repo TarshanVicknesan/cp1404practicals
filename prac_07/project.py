a88ce23b7436208621ab2c43a92683e5d0ac697f
Tarshan_testing <vicknesantarshan@gmail.com>
2023-09-03 19:50:44 +0800

    cp1404practicals

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,13 +1,16 @@
 class Project:
-    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+    def __init__(self, name, start_date, priority, cost_estimate, percent_complete):
         self.name = name
         self.start_date = start_date
         self.priority = priority
         self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
+        self.percent_complete = percent_complete
 
     def __str__(self):
-        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
+        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, " \
+               f"priority: {self.priority}, cost estimate: {self.cost_estimate:.2f}, " \
+               f"completion: {self.percent_complete}%"
 
     def __lt__(self, other):
         return self.priority < other.priority
+
