6d22a4da66b9f099297202f16dd3fd95f76594d3
Tarshan_testing <vicknesantarshan@gmail.com>
2023-09-03 20:09:58 +0800

    cp1404practicals

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -1,8 +1,3 @@
-"""
-CP1404/CP5632 Practical
-File and class example - opens/reads a file, stores in objects of custom class
-(contains multiple versions for demonstration: using csv and namedtuple)
-"""
 
 import csv
 from collections import namedtuple
