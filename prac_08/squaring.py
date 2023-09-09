60ae4d5ec3d12615d2616019266ddfcd17684cad
Tarshan_testing <vicknesantarshan@gmail.com>
2023-09-09 20:23:38 +0800

    cp1404practicals

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -4,22 +4,22 @@ from kivy.core.window import Window
 
 
 class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+    """Kivy App for squaring a number."""
 
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (400, 200)  # Adjusted window size
+        """Build the Kivy app from the kv file."""
+        Window.size = (200, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """Handle calculation (could be button press or other call), output result to label widget."""
         try:
-            result = float(value) ** 2
+            result = int(value) ** 2
             self.root.ids.output_label.text = str(result)
         except ValueError:
-            pass
+            self.root.ids.output_label.text = ""
 
 
 SquareNumberApp().run()
