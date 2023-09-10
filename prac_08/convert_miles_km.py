599359932aa98b2dac900ef82a72db5f65fb2058
Tarshan_testing <vicknesantarshan@gmail.com>
2023-09-10 21:07:08 +0800

    cp1404practicals

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,38 +1,37 @@
 from kivy.app import App
 from kivy.lang import Builder
 
-__author__ = 'Tarshan Vicknesan'
-
 MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
     """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+
     def build(self):
-        """ build the Kivy app from the kv file """
+        """ Build the Kivy app from the kv file """
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculate(self):
-        """ handle calculation (could be button press or other call), output result to label widget """
+    def calculate_conversion(self):
+        """ Perform the conversion calculation """
         value = self.get_validated_miles()
         result = value * MILES_TO_KM
         self.root.ids.output_label.text = str(result)
 
-    def handle_increment(self, change):
+    def update_miles(self, change):
         """
-        handle up/down button press, update the text input with new value, call calculation function
-        :param change: the amount to change
+        Handle up/down button press to update miles and recalculate
+        :param change: The amount to change
         """
         value = self.get_validated_miles() + change
         self.root.ids.input_miles.text = str(value)
-        self.handle_calculate()
+        self.calculate_conversion()
 
     def get_validated_miles(self):
         """
-        get text input from text entry widget, convert to float
-        :return: 0 if error, float version of text if valid
+        Get the miles from the text input widget and convert to float
+        :return: 0 if there's an error, otherwise the float value of the text input
         """
         try:
             value = float(self.root.ids.input_miles.text)
