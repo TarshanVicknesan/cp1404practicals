e5e5c65ae222a46181e0b1314c73523d0753739d
Tarshan_testing <vicknesantarshan@gmail.com>
2023-09-18 15:12:26 +0800

    cp1404practicals

--- prac_01/__init__.py
+++ prac_01/__init__.py
@@ -1,2 +1,16 @@
+# def main():
+#     for i in range(5):
+#         print(do_thing(i))
+#
+#
+# def do_thing(i):
+#     return '**' * i
+#
+#
+# main()
 
 
+text = "O when THE Saints"
+parts = text.lower().split()
+for i, part in enumerate(sorted(parts)):
+    print(part, i)
