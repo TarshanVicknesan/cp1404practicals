d649f766f4d150e68499a77a6b0ca1b4b89df92a
Tarshan_testing <vicknesantarshan@gmail.com>
2023-09-24 11:10:23 +0800

    cp1404practicals

---
+++ prac_10/wiki.py
@@ -0,0 +1,10 @@
+import wikipedia
+
+search_phrase = input("Search phrase: ")
+while search_phrase != '':
+    try:
+        searched_page = wikipedia.page(search_phrase)
+        print(f"Title: {searched_page.title}\nSummary: {wikipedia.summary(search_phrase)}URL: {searched_page.url}\n")
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(e.options)
+    search_phrase = input("Search phrase: ")
