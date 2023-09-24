a1e85a21a8d262b6108b9992189895e656c66138
Tarshan_testing <vicknesantarshan@gmail.com>
2023-09-24 11:25:34 +0800

    cp1404practicals

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,10 +1,14 @@
 import wikipedia
 
 search_phrase = input("Search phrase: ")
+
 while search_phrase != '':
     try:
         searched_page = wikipedia.page(search_phrase)
-        print(f"Title: {searched_page.title}\nSummary: {wikipedia.summary(search_phrase)}URL: {searched_page.url}\n")
+        print(f"Title: {searched_page.title}\nSummary: {wikipedia.summary(search_phrase)}\nURL: {searched_page.url}\n")
     except wikipedia.exceptions.DisambiguationError as e:
         print(e.options)
-    search_phrase = input("Search phrase: ")
+
+    search_phrase = input("Search phrase (blank to quit): ")
+
+print("Goodbye!")
