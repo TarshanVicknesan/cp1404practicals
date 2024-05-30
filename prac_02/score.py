53d849b091a3613b25da55c1d035dff826580bad
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-05-30 10:34:43 +0800

    added main function to score.py and random score generator

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,19 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """main function used to get user input of their score and adds the option to request for a random score"""
+    user_score = float(input("Enter your score: "))
+    result = evaluate_score(user_score)
+    print(result)
+
+    random_score = random.randint(0, 100)
+    result = evaluate_score(random_score)
+    print(f"Random score: {random_score}")
+    print(f"Result for random score: {result}")
+
+
+
