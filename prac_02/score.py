d0e525ef111618b83a2936393f59da3a6da9f3a4
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-06-01 13:45:33 +0800

    added evaluate score function to evaluate grades of each score once input is gathered.

--- prac_02/score.py
+++ prac_02/score.py
@@ -17,4 +17,15 @@ def main():
     print(f"Result for random score: {result}")
 
 
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
+
+main()
