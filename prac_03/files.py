c5a9926b541a5ac2cae0149a9cfd4d7689b67088
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-06-08 17:01:33 +0800

    done task 2, read name file from names.txt done task 3, finished code that opens numbers.txt and adds them to make 59 done task 4, to print the total for all lines in numbers.txt

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,17 +7,26 @@
 
 # Task 2: In the same file, but as if it were a separate program,
 # write code that opens "name.txt" and reads the name (as above) then prints (note the exact output)
-file = open("name.txt", "r")
-name = file.read().strip()
-file.close()
-print(f"Your name is {name}")
+# file = open("name.txt", "r")
+# name = file.read().strip()
+# file.close()
+# print(f"Your name is {name}")
 
 # Task 3: Write code that opens numbers.txt, reads only the first two numbers,
 # adds them together then prints the result,
 # which should be... 59. Use with instead of open and close for this question.
-
+# with open("numbers.txt", "r") as file:
+#     first_number = int(file.readline())
+#     second_number = int(file.readline())
+# result = first_number + second_number
+# print(result)
 
 # Task 4: Now write a fourth block of code that prints the total for all lines in numbers.txt.
 # This should work for a file with any number of numbers.
 # Use with instead of open and close for this question.
+# total = 0
+# with open("numbers.txt", "r") as file:
+#     for line in file:
+#         total += int(line)
+# print(total)
 
