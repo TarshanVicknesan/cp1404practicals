4c683fdeee9e9bcbde38de21a7370525cdafcc1d
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-06-08 16:56:28 +0800

    done task 2, read name file from names.txt

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,13 +1,16 @@
 # Task 1: Write code that asks the user for their name,
 # then opens a file called name.txt and writes that name to it. Use open and close for this question.
-name = input("Please enter your name: ")
-file = open("name.txt", "w")
-file.write(name)
-file.close()
+# name = input("Please enter your name: ")
+# file = open("name.txt", "w")
+# file.write(name)
+# file.close()
 
 # Task 2: In the same file, but as if it were a separate program,
 # write code that opens "name.txt" and reads the name (as above) then prints (note the exact output)
-
+file = open("name.txt", "r")
+name = file.read().strip()
+file.close()
+print(f"Your name is {name}")
 
 # Task 3: Write code that opens numbers.txt, reads only the first two numbers,
 # adds them together then prints the result,
