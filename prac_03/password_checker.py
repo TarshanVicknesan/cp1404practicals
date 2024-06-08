d9460834ac07387dff7845cc18e9de7e1c6593c9
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-06-08 16:44:08 +0800

    added password_checker.py raw code from sample.

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -3,10 +3,9 @@ CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
 
-# Constants
-MIN_LENGTH = 5
-MAX_LENGTH = 15
-SPECIAL_CHARS_REQUIRED = True
+MIN_LENGTH = 2
+MAX_LENGTH = 6
+IS_SPECIAL_CHARACTER_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -17,48 +16,33 @@ def main():
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters:", SPECIAL_CHARACTERS)
-
-    while True:
+    if IS_SPECIAL_CHARACTER_REQUIRED:
+        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
         password = input("> ")
-        if is_valid_password(password):
-            print(f"Your {len(password)} character password is valid: {password}")
-            break
-        else:
-            print("Invalid password!")
+    print(f"Your {len(password)}-character password is valid: {password}")
 
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # Check the length of the password
-    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
-        return False
+    # TODO: if length is wrong, return False
 
-    # Initialize counters for different types of characters
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
+    number_of_lower = 0
+    number_of_upper = 0
+    number_of_digit = 0
+    number_of_special = 0
+    for character in password:
+        # TODO: count each kind of character (use str methods like isdigit)
+        pass
 
-    # Count the occurrences of each type of character in the password
-    for char in password:
-        if char.islower():
-            count_lower += 1
-        elif char.isupper():
-            count_upper += 1
-        elif char.isdigit():
-            count_digit += 1
-        elif SPECIAL_CHARS_REQUIRED and char in SPECIAL_CHARACTERS:
-            count_special += 1
+    # TODO: if any of the 'normal' counts are zero, return False
 
-    # Check if all required character types are present
-    if count_lower == 0 or count_upper == 0 or count_digit == 0:
-        return False
+    # TODO: if special characters are required, then check the count of those
+    # and return False if it's zero
 
-    if SPECIAL_CHARS_REQUIRED and count_special == 0:
-        return False
-
+    # if we get here (without returning False), then the password must be valid
     return True
 
 
