782d190cbeaa4199482004bf81243efb3fb1325a
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-06-15 09:51:03 +0800

    completed list_exercises.py that prompts the user for 5 numbers and places them in a list.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,3 +1,14 @@
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+numbers = []
 
+for i in range(5):
+    num = int(input("Number: "))
+    numbers.append(num)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+# usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+#              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
