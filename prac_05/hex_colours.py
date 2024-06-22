3a9d288f14c1eefd299c09e60a0abfadf4a01caf
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-06-22 20:45:48 +0800

    finished hex_colours.py following the same format as state_names.py however adding the user to prompt for a colour.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -21,3 +21,31 @@
 #     except KeyError:
 #         print("Invalid short state")
 #     state_code = input("Enter short state: ").upper()
+
+# Define a dictionary with some color names and their corresponding hexadecimal codes
+COLOUR_CODES = {
+    'absolute zero': '#0048ba',
+    'acid green': '#b0bf1a',
+    'aliceblue': '#f0f8ff',
+    'alizarin crimson': '#e32636',
+    'amaranth': '#e52b50',
+    'amber': '#ffbf00',
+    'amethyst': '#9966cc',
+    'antiquewhite': '#faebd7',
+    'antiquewhite1': '#ffefdb',
+    'antiquewhite2': '#eedfcc'
+}
+
+# Loop to print the dictionary
+for name, code in COLOUR_CODES.items():
+    print(f"{name:<14} is {code}")
+
+# Prompt the user to enter a color name
+color_name = input("Enter color name: ").lower()
+while color_name != "":
+    try:
+        # Try to print the color code using the EAFP approach
+        print(f"{color_name} is {COLOUR_CODES[color_name]}")
+    except KeyError:
+        print("Invalid color name")
+    color_name = input("Enter color name: ").lower()
