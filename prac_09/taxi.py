6261ed092d121994247f6fb9c34874d7d664508f
TarshanVicknesan <tarshan.vicknesan@my.jcu.edu.au>
2024-07-30 13:48:06 +0800

    edited taxi_test.py and taxi.py to incorporate new object my_taxi.

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -1,33 +1,34 @@
 """
-CP1404/CP5632 Practical
-Car class
+CP1404 Practical
 """
+
 from prac_09.car import Car
 
 
 class Taxi(Car):
-    """Specialised version of a Car that includes fare costs."""
+    """Specialised Car with fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
-        """Initialise a Taxi instance, based on parent class Car."""
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel):
+        """Initialise Taxi with name and fuel."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
-        """Return a string like a Car but with current fare distance."""
+        """Return string with fare details."""
         return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
     def get_fare(self):
-        """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        """Calculate fare."""
+        return round(self.price_per_km * self.current_fare_distance, 1)
 
     def start_fare(self):
-        """Begin a new fare."""
+        """Reset fare distance."""
         self.current_fare_distance = 0
 
     def drive(self, distance):
-        """Drive like parent Car but calculate fare distance as well."""
+        """Drive and update fare distance."""
         distance_driven = super().drive(distance)
         self.current_fare_distance += distance_driven
         return distance_driven
